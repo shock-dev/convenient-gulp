959efcd85d01bc2826b49f9eaf6ff982bccb278b
Andrey <shock.work@mail.ru>
2020-06-30 02:04:32 +0300

    improved functionality

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,31 @@
 let gulp = require('gulp'),
     sass = require('gulp-sass'),
-    browserSync = require('browser-sync');
+    browserSync = require('browser-sync'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename');
 
-gulp.task('scss', function () {
-   return gulp.src('app/scss/**/*.scss')
-       .pipe(sass({outputStyle: 'expended'}))
+gulp.task('sass', function () {
+   return gulp.src('app/sass/**/*.sass')
+       .pipe(sass({outputStyle: 'compressed'}))
+       .pipe(rename({
+          suffix: '.min'
+       }))
        .pipe(gulp.dest('app/css'))
        .pipe(browserSync.reload({stream: true}))
 });
 
 gulp.task('html', function () {
    return gulp.src('app/**/*.html')
+       .pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('js', function () {
+   return gulp.src('app/js/main.js')
+       .pipe(uglify())
+       .pipe(rename({
+           suffix: '.min'
+       }))
+       .pipe(gulp.dest('app/js/'))
        .pipe(browserSync.reload({stream: true}))
 });
 
@@ -23,8 +38,9 @@ gulp.task('browser-sync', function() {
 });
 
 gulp.task('watch', function () {
-   gulp.watch('app/scss/**/*.scss', gulp.parallel('scss'));
+   gulp.watch('app/sass/**/*.sass', gulp.parallel('sass'));
    gulp.watch('app/**/*.html', gulp.parallel('html'));
+   gulp.watch('app/js/**/*.js', gulp.parallel('js'))
 });
 
-gulp.task('default', gulp.parallel('browser-sync', 'watch'));
+gulp.task('default', gulp.parallel('sass', 'js', 'browser-sync', 'watch'));
