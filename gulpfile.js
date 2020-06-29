627d9091746c677dedb522ab52eff124177110f8
Andrey <shock.work@mail.ru>
2020-06-29 22:14:26 +0300

    added scss

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+let gulp = require('gulp'),
+    sass = require('gulp-sass');
+
+gulp.task('scss', function () {
+   return gulp.src('app/scss/**/*.scss')
+       .pipe(sass())
+       .pipe(gulp.dest('app/css'))
+});
