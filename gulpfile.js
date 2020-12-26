9519a2cfd614259d44cc825ec381550ec8bbf933
Andrey <shock.work@mail.ru>
2020-12-26 23:22:43 +0300

    watching scss files

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,17 @@
-const { src, dest } = require('gulp')
+const { src, dest, watch } = require('gulp')
 const scss = require('gulp-sass')
 const concat = require('gulp-concat')
 
-const styles = () => src('./src/scss/index.scss')
-    .pipe(scss({ outputStyle: 'compressed' }))
-    .pipe(concat('index.min.css'))
-    .pipe(dest('./dist/css'))
+function styles() {
+    return src('./src/scss/index.scss')
+        .pipe(scss({ outputStyle: 'compressed' }))
+        .pipe(concat('index.min.css'))
+        .pipe(dest('./dist/css'))
+}
+
+function watching() {
+    watch(['src/scss/**/*.scss'], styles)
+}
 
 exports.styles = styles
+exports.watching = watching
