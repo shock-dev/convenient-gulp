0e5c30a0f63a5fe9b3beec2ec3e2128d6c268767
Andrey <shock.work@mail.ru>
2020-12-27 17:52:30 +0300

    improve working with styles + include html files

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,53 @@
-const { src, dest, watch } = require('gulp')
+const { src, dest, watch, parallel } = require('gulp')
 const scss = require('gulp-sass')
-const concat = require('gulp-concat')
+const notify = require('gulp-notify')
+const sourcemaps = require('gulp-sourcemaps')
+const rename = require('gulp-rename')
+const autoprefixer = require('gulp-autoprefixer')
+const cleanCSS = require('gulp-clean-css')
+const browserSync = require('browser-sync').create()
+const fileinclude = require('gulp-file-include')
 
-function styles() {
-    return src('./src/scss/index.scss')
-        .pipe(scss({ outputStyle: 'compressed' }))
-        .pipe(concat('index.min.css'))
+const styles = () => (
+    src('./src/scss/index.scss')
+        .pipe(sourcemaps.init())
+        .pipe(scss({
+            outputStyle: 'expanded'
+        }).on('error', notify.onError()))
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(autoprefixer({
+            cascade: false
+        }))
+        .pipe(cleanCSS({
+            level: 2
+        }))
+        .pipe(sourcemaps.write('.'))
         .pipe(dest('./dist/css'))
-}
+        .pipe(browserSync.stream())
+)
 
-function watching() {
-    watch(['src/scss/**/*.scss'], styles)
+const htmlInclude = () => (
+    src('./src/index.html')
+        .pipe(fileinclude({
+            prefix: '@',
+            basepath: '@file'
+        }))
+        .pipe(dest('./dist'))
+        .pipe(browserSync.stream())
+)
+
+const watchFiles = () => {
+    browserSync.init({
+        server: {
+            baseDir: './dist'
+        }
+    })
+
+    watch('./src/scss/**/*.scss', styles)
+    watch('./src/index.html', htmlInclude)
 }
 
 exports.styles = styles
-exports.watching = watching
+exports.watchFiles = watchFiles
