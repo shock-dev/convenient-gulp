5f08cb0bf5916b9147971c32b4da164741343a68
Andrey <shock.work@mail.ru>
2020-12-27 19:48:21 +0300

    fonts -> woff & woff2

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-const { src, dest, watch, parallel, series } = require('gulp')
+const { src, dest, watch, series } = require('gulp')
 const scss = require('gulp-sass')
 const notify = require('gulp-notify')
 const sourcemaps = require('gulp-sourcemaps')
@@ -8,6 +8,18 @@ const cleanCSS = require('gulp-clean-css')
 const browserSync = require('browser-sync').create()
 const fileinclude = require('gulp-file-include')
 const svgSprite = require('gulp-svg-sprite')
+const ttf2woff = require('gulp-ttf2woff')
+const ttf2woff2 = require('gulp-ttf2woff2')
+
+const fonts = () => {
+    src('./src/fonts/**.ttf')
+        .pipe(ttf2woff())
+        .pipe(dest('./dist/fonts'))
+
+    return src('./src/fonts/**.ttf')
+        .pipe(ttf2woff2())
+        .pipe(dest('./dist/fonts'))
+}
 
 const svgSprites = () => (
     src(['./src/img/**.svg'])
@@ -67,9 +79,10 @@ const watchFiles = () => {
     watch('./src/index.html', htmlInclude)
     watch(['./src/img/**.jpg', './src/img/**.jpeg', './src/img/**.png'], imgToDist)
     watch(['./src/img/**.svg'], svgSprites)
+    watch(['./src/fonts/**.ttf'], fonts)
 }
 
 exports.styles = styles
 exports.watchFiles = watchFiles
 
-exports.default = series(htmlInclude, styles, imgToDist, svgSprites, watchFiles)
+exports.default = series(htmlInclude, fonts, styles, imgToDist, svgSprites, watchFiles)
